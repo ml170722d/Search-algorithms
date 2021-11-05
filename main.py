29c96e1f05bb4e904204d96e0a28c8585dfb91e5
Luka <ml170722.etf@gmail.com>
2021-11-05 03:09:10 +0100

    display weight map on start of game

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ from game import Game
 try:
     pygame.init()
     g = Game()
+
+    print()
+    print('--', end='')
+    print('-'*5*len(g.tile_map[0]))
+    for row in g.tile_map:
+        print('| ', end='')
+        for tile in row:
+            print("{:<5}".format(tile.cost()), end='')
+        print('|')
+    print('-', end='')
+    print('-'*5*len(g.tile_map[0]))
+
     g.run()
 except (Exception,):
     traceback.print_exc()
