9e987809c916113d37e6cc0cf617560dacd3bb43
Luka <ml170722.etf@gmail.com>
2021-11-07 20:42:21 +0100

    Visual solutions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys, re
 import traceback
 import pygame
 
@@ -9,18 +10,21 @@ try:
 
     print()
     print('--', end='')
-    print('-'*5*len(g.tile_map[0]))
+    print('-' * 5 * len(g.tile_map[0]))
     for row in g.tile_map:
         print('| ', end='')
         for tile in row:
             print("{:<5}".format(tile.cost()), end='')
         print('|')
     print('-', end='')
-    print('-'*5*len(g.tile_map[0]))
+    print('-' * 5 * len(g.tile_map[0]))
 
     g.run()
 except (Exception,):
     traceback.print_exc()
     input()
 finally:
+    # map_arg = re.compile('.*(map\\d).txt').match(sys.argv[1]).group(1)
+    # agent_arg = sys.argv[2]
+    # pygame.image.save(g.screen.copy(), "./solutions/{agent}-{map}.png".format(agent=agent_arg, map=map_arg))
     pygame.quit()
